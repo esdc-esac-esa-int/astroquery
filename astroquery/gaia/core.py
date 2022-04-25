beaca1034e047e4d42742fe8c21910cef8079a49
Maria Henar Sarmiento <mhsarmiento@sciops.esa.int>
2022-04-25 14:10:02 +0200

    GAIAPCR-1151: As requested by S/AS, new update on the help content for param 'valid_data' at method 'load_data'

--- astroquery/gaia/core.py
+++ astroquery/gaia/core.py
@@ -190,10 +190,11 @@ class GaiaClass(TapPlus):
             For future GAIA DR3 (Once published), possible values will be ['EPOC_PHOTOMETRY', 'RVS', 'XP_CONTINUOUS',
             'XP_SAMPLED', 'MCMC_GSPPHOT' or 'MCMC_MSC']
         valid_data : bool, optional, default False
-            By default, the epoch photometry service returns all available data, including data rows where
-            flux is null and/or rejected_by_photometry flag is set to True. In order to retrieve only valid data
-            (data rows where flux is not null and/or rejected by photometry is set to False) this request parameter
-            should be included with valid_data=True.
+            By default, the epoch photometry service returns all available data, including
+            data rows where flux is null and/or the rejected_by_photometry flag is set to True.
+            In order to retrieve only valid data (data rows where flux is not null and/or the
+            rejected_by_photometry flag is set to False) this request parameter should be included
+            with valid_data=True.
         band : str, optional, default None, valid values: G, BP, RP
             By default, the epoch photometry service returns all the
             available photometry bands for the requested source.
